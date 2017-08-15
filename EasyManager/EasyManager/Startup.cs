8bcf5737453e56c955d0fc4e4b2b86f7c9518524
David Satande <dsatande@shawatech.co.za>
2017-08-15 10:23:48 +0200

    My first commit

---
+++ EasyManager/EasyManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EasyManager.Startup))]
+namespace EasyManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
